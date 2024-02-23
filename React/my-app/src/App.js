f9dd3eed13bbc51781e15f37cca8b5067af2cfbf
Ekoyon <avosepeace@gmail.com>
2024-02-22 21:23:26 -1200

    learning react

--- React/my-app/src/App.js
+++ React/my-app/src/App.js
@@ -1,16 +1,17 @@
 import Todo from './components/Todo'
-import Modal from "./components/Modal";
-import Backdrop from "./components/Backdrop";
+// import Modal from "./components/Modal";
+// import Backdrop from "./components/Backdrop";
 
 function App() {
-    return (<div>
+    return (
+        <div>
             <h1>My Todos</h1>
-            <Todo text='Read my books' />
+            <Todo text='Read my books'/>
             {/*        if i wanted multiple todos, I just need to replicate, like so:*/}
-            <Todo text ='Finish this sprint' />
-            <Todo text='Write more' />
-            <Modal />
-            <Backdrop />
+            <Todo text='Finish this sprint'/>
+            <Todo text='Write more'/>
+            {/*<Modal />*/}
+            {/*<Backdrop />*/}
         </div>
     );
 }
