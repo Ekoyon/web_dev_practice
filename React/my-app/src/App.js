7dedb49920d6f097c5edd65b0474babd31a464da
Ekoyon <avosepeace@gmail.com>
2024-02-25 22:22:01 +0100

    another project

--- React/my-app/src/App.js
+++ React/my-app/src/App.js
@@ -5,10 +5,20 @@ import Todo from './components/Todo'
 import { Route} from 'react-router-dom';
 import AllMeetUps from "./pages/AllMeetUps";
 import NewMeetUp from "./pages/NewMeetUp";
+import Favourites from "./pages/Favourites";
 
 function App() {
     return (
         <div>
+            <Route path='/'>
+                <AllMeetUps />
+            </Route>
+            <Route path='/new-meetup'>
+                <NewMeetUp/>
+            </Route>
+            <Route path='/favourites'>
+                <Favourites />
+            </Route>
             {/*<h1>My Todos</h1>*/}
             {/*<Todo text='Read my books'/>*/}
             {/*/!*        if i wanted multiple todos, I just need to replicate, like so:*!/*/}
