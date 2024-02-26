f65b10f1e9b8ab2c0c2b8f069d0aa86bf2356cd9
Ekoyon <avosepeace@gmail.com>
2024-02-26 23:43:40 +0100

    another project

--- React/another-app/src/App.js
+++ React/another-app/src/App.js
@@ -1,22 +1,24 @@
-import { Route} from 'react-router-dom';
+import {Route, Switch} from 'react-router-dom';
 import AllMeetUps from "./pages/AllMeetUps";
 import NewMeetUp from "./pages/NewMeetUp";
 import Favourites from "./pages/Favourites";
 
 function App() {
-  return (
-      <div>
-        <Route path='/'>
-          <AllMeetUps />
-        </Route>
-        <Route path='/new-meetup'>
-          <NewMeetUp/>
-        </Route>
-        <Route path='/favourites'>
-          <Favourites />
-        </Route>
-      </div>
-  );
+    return (
+        <div>
+            <Switch>
+                <Route path='/'>
+                    <AllMeetUps/>
+                </Route>
+                <Route path='/new-meetup'>
+                    <NewMeetUp/>
+                </Route>
+                <Route path='/favourites'>
+                    <Favourites/>
+                </Route>
+            </Switch>
+        </div>
+    );
 }
 
 export default App;
