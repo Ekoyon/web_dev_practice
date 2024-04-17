9aeb9dcc066852c14fceab371ef3c1973c563528
Ekoyon <avosepeace@gmail.com>
2024-04-17 11:04:03 +0100

    Javascript basics

---
+++ Javascript/index.js
@@ -0,0 +1,25 @@
+//Primitive types in js: Numbers, string, boolean, null and undefined
+
+//things to note for numbers:
+//we've got operators: +,-,*,%
+//There's also NaN that stands for not a number. JS registers as a number, used for impossible operations like 0/0 and all
+// so a number in JS but not a number.
+// using the keyword 'typeof (value)' would give you the data type eg typeof 5 would output number on the console.
+
+// variables are a way of assigning values in JS to come back and update it or use it in a couple of different ways.
+// how to create variables: 'let (variable) = (value);
+// Define a variable called myLuckyNumber and set it equal to 37.
+// let myLuckyNumber = 37;
+// Define a variable called octopusLimbs and set it equal to 8.
+// let octopusLimbs = 8;
+
+// updating a variable:
+// given a variable 'let score = 5;' we could update by 'let score = score +5;' but a shorter syntax would be
+// 'score += 5;'
+
+//sidebar: difference between writing 'i++' and '++i' in code:
+// the first (i++) increases, returns the value stored prior to the increment and then stores the increment
+// the second (++i) increases, stores and returns the incremented value
+// I'll be using ++i sha
+
+//
