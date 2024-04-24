60c3f2e881b43289611ac3654cf64711b3348333
Ekoyon <avosepeace@gmail.com>
2024-04-24 22:38:07 +0100

    conditional

--- Javascript/index.js
+++ Javascript/index.js
@@ -2,17 +2,19 @@ console.log("HELLO FROM THE FIRST JS FILE");
 let total = 1 + 3;
 console.log("GOODBYE!");
 
-let random = Math.random();
-if (random < 0.5) {
-    console.log("MATH WORKS")
-    console.log(random)
-}
+
 
 // if statement
 let ratings = 5;
 
 if (ratings === 5){
     console.log ("wow!");
+}
+
+let random = Math.random();
+if (random < 0.5) {
+    console.log("MATH WORKS")
+    console.log(random)
 }
 
 // if else statement
