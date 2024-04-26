7933ee9eed74e1922fb0c71a150a9966926707a9
Ekoyon <avosepeace@gmail.com>
2024-04-26 10:08:49 +0100

    exercise

--- Javascript/index.js
+++ Javascript/index.js
@@ -58,3 +58,27 @@ if (age < 5){
 } else {
     console.log(" You pay $20 Senior")
 }
+
+// exercise
+// Your job is to print out a color based upon the following rules:
+//
+// if phrase is 'stop', you should print out 'red'
+// if phrase is 'slow', you should print out 'yellow'
+// if phrase is 'go', you should print out 'green'
+// if phrase is anything else, you should print out 'purple'
+
+// solution
+function getColor(phrase){
+    //WRITE YOUR CODE BETWEEN THIS LINE: ↓ ↓ ↓ ↓
+    if (phrase === 'stop') {
+        console.log('red')
+    } else if(phrase === 'slow') {
+        console.log('yellow')
+    } else if(phrase === 'go') {
+        console.log('green')
+    } else {
+        console.log('purple')
+    }
+
+    //AND THIS LINE ↑↑↑↑↑
+}
