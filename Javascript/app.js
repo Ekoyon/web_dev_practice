1cb0953f8d7a0373a4fcbe157258891f37176dde
Ekoyon <avosepeace@gmail.com>
2024-04-26 10:34:49 +0100

    tagging

--- Javascript/app.js
+++ Javascript/app.js
@@ -1,4 +1,6 @@
 const password = prompt("please enter your password.");
+//conventional
+
 // length must be >= 6
 if (password.length >= 6) {
     console.log("right format")
@@ -13,7 +15,7 @@ if (password.indexOf(' ') === -1) {
     console.log('do not add space')
 }
 
-//actual nesting
+// nesting conditionals
 if (password.length >= 6) {
     if (password.indexOf(' ') === -1) {
         console.log('nice and good')
