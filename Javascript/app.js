b22de779dd819f0f22f0f4a0a636e21e200a6136
Ekoyon <avosepeace@gmail.com>
2024-04-26 11:13:29 +0100

    logic & assignment

--- Javascript/app.js
+++ Javascript/app.js
@@ -47,4 +47,14 @@ if (password.length >= 6 && password.indexOf(' ') === -1) {
     console.log('invalid')
 }
 
+// exercise
+//Please change the value of mystery so that the conditional is true and "YOU GOT IT" is printed to the console.  Do not change anything other than the value of mystery on line 1!
+const mystery = 'Paramed7'; //CHANGE THIS VALUE TO MAKE THE CONDITIONAL BELOW TRUE
+
+
+// LEAVE THIS CODE ALONE! (pretty please)
+if (mystery[0] === 'P' && mystery.length > 5 && mystery.indexOf('7') !== -1) {
+    console.log("YOU GOT IT!!!");
+}
+
 // Logical OR
