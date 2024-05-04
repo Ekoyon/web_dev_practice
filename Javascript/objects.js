ac6507af2e3e28151f7f92f1ad32a600ab03300d
Ekoyon <avosepeace@gmail.com>
2024-05-04 22:04:43 +0100

    creating the first object and exercise.

--- Javascript/objects.js
+++ Javascript/objects.js
@@ -1,3 +1,31 @@
 // Object Literal
 // Our second data structure
-// you can store anything on there
+// you can store anything on there, having a key and value (excited about this one)
+// lets make our first
+
+const Ise = {
+    firstName: 'Iseoluwa',
+    lastName: 'Onadipe'
+}
+
+// You can store anything as a value
+
+// exercise
+// Please define a new variable called product.  It should be an object literal with the following properties:
+//
+// name - set to the string "Gummy Bears"
+//
+// inStock - set to the boolean true
+//
+// price  - set to the number 1.99
+//
+// flavors - set to an array of at least three strings like ["grape", "apple", "cherry"]
+
+// solution:
+// Your code goes in here:
+const product = {
+    name: 'Gummy Bears',
+    inStock: true,
+    price: 1.99,
+    flavors: ['grape', 'apple', 'cherry']
+}
