75a54c5ed566f1d4b6301246141f33486583530c
Ekoyon <avosepeace@gmail.com>
2024-05-05 20:09:41 +0100

    exercise

--- Javascript/objects.js
+++ Javascript/objects.js
@@ -41,3 +41,26 @@ const years = {1999: 'Meh',
     2020: 'Iconic'
 }
 // every key is turned into a string and that's why we can do Ise["firstName"]; without the quotes, it won't work cause it expects a variable name when declared with quotes
+
+// exercise
+// Your task to is to create a variable named fullAddress that points to a string using the information from restaurant.
+//
+// fullAddress should point to a string that includes the address, city, state, and zipcode from the restaurant object. Make sure to add any necessary commas or spaces between the values as seen in the exact expected output format shown below.
+//
+// To make it harder to "cheat" by copy and pasting, I've randomly generated the address portion.    For example, if address was "64 Johnson Ave", the fullAddress variable would look like this:
+//
+// "64 Johnson Ave, Brooklyn, NY 11206" // this is the expected output format of the fullAddress string (keep an eye on the commas and/or spaces between the values and match that too!)
+
+// solution
+//PLEASE DON'T TOUCH THIS LINE!
+const restaurant = {
+    name: 'Ichiran Ramen',
+    address: `${Math.floor(Math.random() * 100) + 1} Johnson Ave`,
+    city: 'Brooklyn',
+    state: 'NY',
+    zipcode: '11206',
+}
+
+//YOUR CODE GOES DOWN HERE:
+let fullAddress = restaurant.address + ", " + restaurant.city + ", " + restaurant.state + " " + restaurant.zipcode;
+restaurant[fullAddress];
