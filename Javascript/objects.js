689794e43878f2f6c038376f606136769e4c9016
Ekoyon <avosepeace@gmail.com>
2024-05-14 18:13:24 +0100

    another example of nested for loop

--- Javascript/objects.js
+++ Javascript/objects.js
@@ -138,12 +138,12 @@ for (let i = 100; i >= 10; i -= 10) {
 // Break keyword
 // Iterating objects
 // Nesting
-for (let i=0; i <= 5; i--) {
-    console.log(`i is ${i}`)
-    for (let j = 1; j < 4; j++) {
-        console.log(`j is ${j}`)
-    }
-}
+// for (let i=0; i <= 5; i--) {
+//     console.log(`i is ${i}`)
+//     for (let j = 1; j < 4; j++) {
+//         console.log(`j is ${j}`)
+//     }
+// }
 // results
 // i is 1
 // j is    1
@@ -186,3 +186,17 @@ for (let i=0; i <= 5; i--) {
 // j is    2
 // j is    3
 
+// another example, you'll use a nested for loop for nested arrays also.
+const seatingChart = [
+    ['Kristen', 'Erik', 'Namita'],
+    ['Geoffrey', 'Juanita', 'Antonio', 'Kevin'],
+    ['Yuma', 'Yuto', 'Sakute', 'Jack', 'Kim']
+]
+// if I wanted to print names according to seating arrangement, one after another:
+for (let i = 0; i < seatingChart.length; i++) {
+    const row = seatingChart[i];
+    console.log(`ROW ${i + 1}`)
+    for (let j =0; j < row.length; j++){
+        console.log(j);
+    }
+}
