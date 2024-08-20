6c84ca080f45226d1e6594d1fadd62ef33f2d3e1
Ekoyon <avosepeace@gmail.com>
2024-08-20 17:13:43 +0100

    new work

---
+++ work/console.js
@@ -0,0 +1,11 @@
+const randomClickFunction = function () {
+    const colors = ["green", "lemon", "blue", "purple", "red", "orange", "yellow", "black", "grey", "white"];
+  
+  const randomIndex = Math.floor(Math.random() * colors.length);
+  
+  const randomColor = colors[randomIndex];
+  
+  htmlBody.style.backgroundColor = randomColor;
+  
+  console.log("The user clicked and set the colort to" + randomColor);
+  }
