3b1c407eafc598ef78b2636786c01f8cc58508bb
Ekoyon <avosepeace@gmail.com>
2025-05-05 13:35:46 +0100

    validation for inputs

--- backend!/server.js
+++ backend!/server.js
@@ -15,7 +15,31 @@ app.get("/login", (req, res) => {
 })
 
 app.post("/register", (req, res) => {
-    console.log(req.body)
+    const errors = []
+
+
+    if(typeof req.body.username !== "string") req.body.username = ""
+    if(typeof req.body.password !== "string") req.body.password = ""
+
+    req.body.username = req.body.username.trim()
+
+
+    if(!req.body.username){
+        errors.push("This cannot be blank")
+    }
+    if(req.body.username && req.body.username.length < 3){
+        errors.push("Username cannot be less than 3 characters")
+    }
+
+    if(req.body.username && req.body.username.length > 10){
+        errors.push("Username cannot be more than 10 characters")
+    }
+
+    if(req.body.username && !req.body.username.match( /^[a-zA-Z0-9]/+$ )) {
+        errors.push("Username should contain letters and numbers")
+    }
+
+    if(req.body.username)
     res.send("Saved! You now have an account")
 })
 
